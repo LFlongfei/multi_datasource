d13c694d2342fcf1b43bd3b925f46f8cf62fd5d3
weilonglong <weilonglong@txp-life.com>
2020-09-05 21:41:40 +0800

    项目初始化

---
+++ src/test/java/com/longfei/multi_datasource/MultiDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longfei.multi_datasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
